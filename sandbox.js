f2df6e86f17108212ed45fc74eb0ec4bf8a5cd54
malarc <email@email.com>
2020-09-16 18:36:35 -0400

    end of basics

---
+++ sandbox.js
@@ -0,0 +1,14 @@
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+//character = 20;
+character = 'luigi';
+//age = 'yoshi';
+age = 40;
+// isBlackBelt = 'yes'
+isBlackBelt = true;
+// can change value but not TYPE!
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
