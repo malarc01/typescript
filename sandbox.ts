c4b16ce8de77300603b1bcccc7e3c52db6e076eb
malarc <email@email.com>
2020-09-17 11:25:38 -0400

    dyanmic (any) types end

--- sandbox.ts
+++ sandbox.ts
@@ -1,38 +1,23 @@
-// explict types
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+let age: any;
 
-// age = 'luigi';
-age = 30;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'luigi' };
+console.log(age);
 
-// isLogged = 25;
-isLoggedIn = true;
+let mixed: any[] = [];
 
-// arrays
-let ninjas: string[] = [];
-
-ninjas.push('shaun');
-console.log(ninjas);
-// union types
-let mixed: (string | number | boolean)[] = [];
-mixed.push('hello');
-mixed.push(20);
+mixed.push(5);
+mixed.push('mario');
 mixed.push(false);
-console.log({ mixed });
 console.log(mixed);
 
-let uid: string | number;
-uid = '123';
-uid = 123;
+let ninja: { name: any; age: any };
 
-// objects
-let ninjaOne: object;
-ninjaOne = { name: 'yoshi', age: 30 };
-let ninjaTwo: {
-	name: string;
-	age: number;
-	beltColour: string;
-};
+ninja = { name: 'yoshi', age: 25 };
+console.log(ninja);
 
-ninjaTwo = { name: 'mario', age: 20, beltColour: 'black' };
+ninja = { name: 25, age: 'yoshi' };
+console.log(ninja);
