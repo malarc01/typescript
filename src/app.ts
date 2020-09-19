57b28f4acc6d76c2ddfb247f3e198be9ad1f7693
malarc <email@email.com>
2020-09-19 09:17:23 -0400

    end of modules

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,9 @@
-// classes
-class Invoice {
-	// readonly client: string;
-	// private details: string;
-	// public amount: number;
-
-	constructor(readonly client: string, private details: string, public amount: number) {}
-
-	format() {
-		return `${this.client} owes ${this.amount} for ${this.details}`;
-	}
-}
+import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
 let invoices: Invoice[] = [];
-
 invoices.push(invOne);
 invoices.push(invTwo);
 
---
+++ src/classes/Invoice.ts
@@ -0,0 +1,8 @@
+// classes
+export class Invoice {
+	constructor(readonly client: string, private details: string, public amount: number) {}
+
+	format() {
+		return `${this.client} owes ${this.amount} for ${this.details}`;
+	}
+}
