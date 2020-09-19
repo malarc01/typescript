7aa1cd234db7daa833e51d31a11b644358285b3f
malarc <email@email.com>
2020-09-19 09:48:13 -0400

    end of interfaces with classes

--- public/app.js
+++ public/app.js
@@ -1,20 +1,13 @@
-const me = {
-    name: 'Marley',
-    age: 22,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('I spent', amount);
-        return amount;
-    }
-};
-const greetPerson = (person) => {
-    console.log('hello', person.name);
-};
-greetPerson(me);
-console.log(me);
 import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+let docOne;
+let docTwo;
+docOne = new Invoice('yoshi', 'web work', 250);
+docTwo = new Payment('mario', 'plumbing work', 200);
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
+console.log(docs);
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
@@ -31,5 +24,12 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -1,5 +1,7 @@
+import { HasFormatter } from '../interfaces/HasFormatter.js';
+
 // classes
-export class Invoice {
+export class Invoice implements HasFormatter {
 	constructor(readonly client: string, private details: string, public amount: number) {}
 
 	format() {
