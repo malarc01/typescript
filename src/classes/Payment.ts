4676a899326b7c07c9f327cc05dc6179db3cafc1
malarc <email@email.com>
2020-09-26 08:33:36 -0400

    end of rendering html template

--- src/classes/Payment.ts
+++ src/classes/Payment.ts
@@ -2,7 +2,10 @@ import { HasFormatter } from '../interfaces/HasFormatter.js';
 
 // classes
 export class Payment implements HasFormatter {
-	constructor(readonly recipient: string, private details: string, public amount: number) {}
+	constructor(
+		readonly recipient: string,
+		private details: string,
+		public amount: number) {}
 
 	format() {
 		return `${this.recipient} is owed ${this.amount} for ${this.details}`;
